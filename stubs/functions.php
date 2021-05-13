e2f0a2c977cf4f71260f266e84a368ea0f222b4f
James Mannion <james.mannion@lendable.co.uk>
2021-05-13 12:02:25 +0100

    add global namespace and extra functions

--- stubs/functions.php
+++ stubs/functions.php
@@ -2,11 +2,29 @@
 
 declare(strict_types=1);
 
+namespace {
+
+    function dd_trace_peek_span_id(): int
+    {
+        return 0;
+    }
+
+    function ddtrace_config_distributed_tracing_enabled(): bool
+    {
+        return false;
+    }
+}
+
 namespace DDTrace {
     /**
      * @phpstan-param \Closure(DDTrace\SpanData, list<mixed>=, mixed|null=, \Throwable|null=): void $tracingClosure
      */
     function trace_method(string $className, string $methodName, \Closure $tracingClosure): void
     {
+    }
+
+    function trace_id(): int
+    {
+        return 0;
     }
 }
